81f8944b5aa4c4d2943a68d3c90b77c1042c2a2f
4ndr3224 <32282846+drelocatelli@users.noreply.github.com>
2023-04-04 10:40:18 -0300

    Update automationSetup.ts

--- bootstrap/automationSetup.ts
+++ bootstrap/automationSetup.ts
@@ -23,6 +23,64 @@ abstract class AutomationTestSetup {
         const files = fs.readdirSync(dir);
         return files.filter(f => !f.startsWith('basic')).map(t => t = t.replace('.spec.ts', ''));
     }
+    
+    randomNumber(quantity: number) {
+        let num = '';
+        for (let i = 0; i < quantity; i++) {
+            num += Math.floor(Math.random() * 10);
+        }
+        let randomNum = BigInt(num);
+        return randomNum.toString();
+    }
+
+    generateRandomUser(charactersQuantity: number) : string {
+        let username = '';
+        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        const charactersLength = characters.length;
+
+        for (let i = 0; i < charactersQuantity; i++) {
+            username += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+
+        return username;
+    }
+    
+    async readConsole(question: string) {
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout,
+        });
+
+        return new Promise((resolve) => {
+            rl.question(question, (answer) => {
+                rl.close();
+                resolve(answer.toLowerCase());
+            });
+        });
+    }
+
+    async readDialog(page: Page, message: string, type: 'alert' | 'prompt' | 'confirm' = 'alert') {
+        return await page.evaluate(
+            async (type, message) => {
+                let dialog: any = undefined;
+                switch (type) {
+                    case 'alert':
+                        dialog = window.alert(message);
+                        break;
+                    case 'confirm':
+                        dialog = window.confirm(message);
+                        break;
+                    case 'prompt':
+                        dialog = window.prompt(message);
+                        break;
+                }
+
+                return dialog;
+            },
+            type,
+            message,
+        );
+    }
 
 
 }
